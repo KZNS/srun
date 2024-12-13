834ed7bdf8af7a0279213ab150faa486eac22b7a
KZNS <kzns@foxmail.com>
2024-12-14 01:09:08 +0800

    Add init.sh

---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+commit_id=$(curl -s https://api.github.com/repos/kzns/srun/branches/main | grep -o '"sha": *"[^"]*"' | head -n 1 | sed 's/"sha": "//;s/"$//' | cut -c1-7)
+
+echo "#!/usr/bin/env -S scala-cli shebang
+//> using repository jitpack
+//> using dep \"com.github.kzns:srun:$commit_id\""
